ba478d8824d1f1e459d9f2c02875496fed19d805
Jamil-coder360 <jamil595nhj@gmail.com>
2026-05-26 13:00:09 +0600

    merge complete

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,13 @@ import { createBrowserRouter } from "react-router";
 import { RouterProvider } from "react-router/dom";
 import Roots from "./component/main/Roots"
 import HomePage from './pages/HomePage';
+import PricingPage from './pages/PricingPage';
+import ServiceDetailPage from './pages/ServiceDetailPage';
+import ProjectDetails from './pages/ProjectDetails';
+import ProjectPage from './pages/ProjectPage';
+import AboutPage from './pages/AboutPage';
+import ServicePage from './pages/ServicePage';
+import NotFoundPage from './pages/NotFoundPage';
 const router = createBrowserRouter([
   {
     path: "/",
@@ -12,6 +19,35 @@ const router = createBrowserRouter([
         index:true,
         element:<HomePage />,
       },
+      {
+        path:"pricing",
+        element:<PricingPage />
+      },
+      {
+        path:"service_detail",
+        element:<ServiceDetailPage />
+      },
+      {
+        path:"project_detail",
+        element:<ProjectDetails />
+      },
+      {
+        path:"projects",
+        element:<ProjectPage />
+      },
+      {
+        path:"about",
+        element: <AboutPage /> ,
+      },
+      {
+        path:"service",
+        element: <ServicePage />
+      },
+      {
+        path: "*",
+        element: <NotFoundPage />
+      }
+
     ]
   },
 ]);
