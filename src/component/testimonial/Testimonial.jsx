9c92392f6b7c3577c94d0b67ff8be333f2cda964
Jamil-coder360 <jamil595nhj@gmail.com>
2026-05-26 12:52:45 +0600

    projectPage update

--- src/component/testimonial/Testimonial.jsx
+++ src/component/testimonial/Testimonial.jsx
@@ -26,7 +26,7 @@ const Testimonial = () => {
   };
 
   return (
-    <Section style={bgImage} className={"py-15 sm:py-23 lg:pt-30 px-5 lg:px-0"}>
+    <Section style={bgImage} className={"py-15 sm:py-23 lg:pt-30 px-5 lg:px-0 overflow-x-hidden"}>
       <Container>
         <div className="grid grid-cols-1 lg:grid-cols-[620px_1fr] gap-15 justify-between ">
           <div className="w-full h-full lg:h-[715px] overflow-hidden">
--- src/pages/ProjectPage.jsx
+++ src/pages/ProjectPage.jsx
@@ -6,46 +6,57 @@ import Car4 from "../assets/P-car.jpg";
 import Car5 from "../assets/B-car.jpg";
 import Car6 from "../assets/G-car.jpg";
 import righticon from "../assets/icon/righticon.svg";
+
 const ProjectPage = () => {
   const projectItems = [
-    { id: 1, image: Car1 },
-    { id: 2, image: Car2 },
-    { id: 3, image: Car3 },
-    { id: 4, image: Car4 },
-    { id: 5, image: Car5 },
-    { id: 6, image: Car6 },
+    { id: 1, image: Car1, title: "Duct work Inspection" },
+    { id: 2, image: Car2, title: "Engine Repair" },
+    { id: 3, image: Car3, title: "Car Washing" },
+    { id: 4, image: Car4, title: "Interior Cleaning" },
+    { id: 5, image: Car5, title: "Wheel Alignment" },
+    { id: 6, image: Car6, title: "Oil Change" },
   ];
 
   return (
     <main className="homepage-wrapper">
+      {/* Hero Section */}
       <section
-        className="relative bg-black text-white py-24 px-10 bg-cover bg-center h-100"
+        className="relative bg-black text-white py-24 px-10 bg-cover bg-center"
         style={{ backgroundImage: `url(${HeroBg})` }}
       >
         <div className="relative z-10 max-w-7xl mx-auto">
           <h1 className="text-4xl font-bold">Project</h1>
-          <nav className="text-sm mt-3 flex items-center gap-1.5">
+
+          <nav className="text-sm mt-3 flex items-center gap-2">
             <span className="text-gray-300">HOME</span>
-            <span className="text-gray-400">
-              {" "}
-              <img src={righticon} alt="icon" className="w-3 h-3" />{" "}
-            </span>
+
+            <img src={righticon} alt="icon" className="w-3 h-3" />
+
             <span>PROJECT</span>
           </nav>
         </div>
       </section>
 
+      {/* Project Cards */}
       <section className="bg-white py-16 px-5 md:px-10">
         <div className="max-w-7xl mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
           {projectItems.map((item) => (
             <div
               key={item.id}
-              className="relative group overflow-hidden bg-gray-100 aspect-[4/3] shadow-md hover:shadow-xl transition-shadow duration-300"
+              className="relative group overflow-hidden rounded-xl shadow-md hover:shadow-xl transition-all duration-300"
             >
-              <img
-                src={item.image}
-                className="w-410px h-490px object-cover group-hover:scale-105 transition-transform duration-500"
-              />
+              <div className="overflow-hidden">
+                <img
+                  src={item.image}
+                  alt={item.title}
+                  className="w-full h-[320px] object-cover group-hover:scale-105 transition-transform duration-500"
+                />
+              </div>
+              <div className=" opacity-0 absolute bottom-4 left-0  group-hover:opacity-100 transition-opacity duration-300">
+                <button className="text-2xl font-bold p-5 tracking-wide bg-black_900 text-white border-r-4 border-r-home">
+                  {item.title}
+                </button>
+              </div>
             </div>
           ))}
         </div>
